bc6b6c95bbd9f615ccdd90d2c74f99954e809c3a
hartmann <vegard.hartmann@gmail.com>
2009-08-08 11:55:42 +0200

    lagt inn nytt søkeeksempel

--- src/test/java/no/hartmann/javazone/google/GoogleSearchTest.java
+++ src/test/java/no/hartmann/javazone/google/GoogleSearchTest.java
@@ -1,10 +1,13 @@
 package no.hartmann.javazone.google;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.Test;
 import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertNotNull;
 
 public class GoogleSearchTest {
 
@@ -15,6 +18,16 @@ public class GoogleSearchTest {
         SearchPage searchPage = createSearchPage(driver);
         SearchResultPage resultPage = searchPage.searchFor("webdriver");
         assertTrue(resultPage.isLinkPresentInResults("http://code.google.com/p/webdriver/"));
+    }
+
+    @Test
+    public void whenSearchingForWebDriverResultPageShouldContainLinkWithWordWebDriver() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com/");
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("webdriver");
+        searchField.submit();
+        assertNotNull(driver.findElement(By.partialLinkText("webdriver")));
     }
 
     private SearchPage createSearchPage(WebDriver driver) {
